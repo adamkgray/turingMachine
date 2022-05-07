1bf0f49e8dcf3bf469c60b05748e8cc0f0f05b7f
Adam K Gray <adamkgray@pm.me>
2022-05-07 14:48:13 +0100

    refactor

---
+++ README.md
@@ -0,0 +1,54 @@
+# Turing Machine
+
+A command-line utility to write and visualise the execution of turing machines.
+
+## Usage
+
+Compile with `go build` to produce the executable called `turingmachine`. Running the executable with the `-h` or `--help` flag prints the help page:
+
+```bash
+$ ./turingmachine --help
+Usage of ./turingmachine:
+  -e string
+        halting state symbol (default "h")
+  -i string
+        string representing input tape
+  -s string
+        initial state symbol (default "s")
+  -t string
+        path to CSV file of turing machine rules (default "table.csv")
+```
+
+- By default, the halting symbol is `h`, but you can override this behavior with the `-e` flag, i.e. `-e H` to make `H` the halting symbol
+- By default, the initial state symbol is `s`, but you can override this behavior with the `-s` flag, i.e. `-s S` to make `S` the halting symbol
+- By default, the program looks for the state transition table in a file called `table.csv`, although you can tell it to look anywhere by specifying a path after the `-t` flag ("t" for table), i.e. `-t /path/to/table.csv`
+- You must specify an input string with the `-i` flag, i.e. `-i 10101101`
+
+## The state transition table
+
+The state transition table is given by a CSV file. Refer to the following rules to create your own:
+
+- The columns from left to right are: state, character read, next state, action
+- Do not include the column names in the csv
+- To read a blank (space), specify `_` in the character read column
+- To erase a charcater on the tape, specify `_` in the action column
+- To write a character on the tape, for example `1`, specify `1` in the action column
+- To move the head right, specify `->` in the action column
+- To lmove the head left, specify `<-` in the action column
+
+Here is an example program which appends `010` to any binary number input greater than `0`:
+```
+s,0,n,_
+n,_,n,1
+n,1,m,->
+m,_,h,0
+s,1,z,<-
+z,^,z,->
+z,0,z,->
+z,1,z,->
+z,_,y,0
+y,0,x,->
+x,_,w,1
+w,1,v,->
+v,_,h,0
+```
--- turingmachine.go
+++ turingmachine.go
@@ -11,6 +11,85 @@ import (
 	"time"
 )
 
+func main() {
+	// Parse command line args
+	initial := flag.String("s", "s", "initial state symbol")
+	halt := flag.String("h", "h", "halting state symbol")
+	input := flag.String("i", "", "string representing input tape")
+	file := flag.String("t", "table.csv", "path to CSV file of turing machine rules")
+	flag.Parse()
+
+	// Prepend the input tape with '^' for clearer visualisation
+	tape := "^" + *input
+
+	// Init the turing machine struct with the initial tape, halting symbol and the head at position 1
+	m := machine{
+		state: *initial,
+		halt:  *halt,
+		head:  1,
+	}
+
+	// Read the state transition table
+	rules := readCsvFile(*file)
+
+	// Parse the state transition table into a map of instructions
+	table := make(map[string]instruction)
+	for _, rule := range rules {
+		// The key is the unique combination of the current state and the character read
+		current := rule[0]
+		read := rule[1]
+		next := rule[2]
+		action := rule[3]
+		key := current + read
+		table[key] = instruction{
+			next:   next,
+			action: action,
+		}
+	}
+
+	// Clear the terminal, display the input tape, and wait
+	clear()
+	display(tape, m.head)
+	wait(250)
+
+	for {
+		// Exit when the turing machine reaches the halting state
+		if m.state == m.halt {
+			break
+		}
+
+		// Clear the terminal
+		clear()
+
+		// Read the state and the character under the head together as a string
+		rule := m.state + string(tape[m.head])
+		// Given that string, pull the associated rules from the table
+		result := table[rule]
+		// Determine the next state
+		m.state = result.next
+
+		if result.action == "<-" { // Move the head to the left
+			if m.head == 0 { // Throw error if the state transition table is buggy
+				panic("attempted to move head past start of tape")
+			}
+			// Decrement the head position
+			m.head--
+		} else if result.action == "->" { // Move the head to the right
+			if m.head == len(tape)-1 { // If the head moves past the end of the tape, we treat it as adding a blank space to the end of the tape
+				tape += "_"
+			}
+			// Increment the head position
+			m.head++
+		} else { // Update or erase the character under the head
+			tape = tape[:m.head] + result.action + tape[m.head+1:]
+		}
+
+		// Display the new state of the tape and wait
+		display(tape, m.head)
+		wait(250)
+	}
+}
+
 func readCsvFile(filePath string) [][]string {
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -31,8 +110,8 @@ func clear() {
 	cmd.Run()
 }
 
-func display(input string, head int) {
-	fmt.Println(input)
+func display(tape string, head int) {
+	fmt.Println(tape)
 	fmt.Println(strings.Repeat(" ", head) + "@")
 }
 
@@ -49,70 +128,4 @@ type machine struct {
 type instruction struct {
 	next   string
 	action string
-}
-
-func main() {
-	initial := flag.String("s", "s", "initial state symbol")
-	halt := flag.String("h", "h", "halting state symbol")
-	input := flag.String("i", "", "input tape")
-	file := flag.String("t", "table.csv", "turing machine rules")
-	flag.Parse()
-
-	tape := "^" + *input
-
-	m := machine{
-		state: *initial,
-		halt:  *halt,
-		head:  1,
-	}
-
-	rules := readCsvFile(*file)
-
-	table := make(map[string]instruction)
-	for _, rule := range rules {
-		// the key is the unique combination of
-		// current state and character read
-		current := rule[0]
-		read := rule[1]
-		next := rule[2]
-		action := rule[3]
-		key := current + read
-		table[key] = instruction{
-			next:   next,
-			action: action,
-		}
-	}
-
-	clear()
-	display(tape, m.head)
-	wait(250)
-
-	for {
-		if m.state == m.halt {
-			break
-		}
-
-		clear()
-
-		rule := m.state + string(tape[m.head])
-		result := table[rule]
-		m.state = result.next
-
-		if result.action == "<-" {
-			if m.head == 0 {
-				panic("attempted to move head past start of tape")
-			}
-			m.head--
-		} else if result.action == "->" {
-			if m.head == len(tape)-1 {
-				tape += "_"
-			}
-			m.head++
-		} else {
-			tape = tape[:m.head] + result.action + tape[m.head+1:]
-		}
-
-		display(tape, m.head)
-		wait(250)
-	}
 }
